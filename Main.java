3d749920ea3099e66f69986dcb6d5ac683cb2733
laurwalker1 <lauren.walker@zinkworks.com>
2022-04-28 12:13:38 +0100

    exercise

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static  void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calculator");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Multiplication (*) ");
+        System.out.println("3. Substraction (-) ");
+        System.out.println("4. Division (/) ");
+
+        System.out.println("Choose an operator: ");
+        char operation = input.next().charAt(0);
+
+        System.out.println("Enter the first number: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        if (operation == '+') {
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        } else if (operation == '-') {
+            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        } else if (operation == '*') {
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        } else if (operation == '/') {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        } else {
+            System.out.println("Invalid, try again!");
+        }
+
+
+
+
+
+    }
+
+}
